2d481f944053cd34ef9f6c3816dcf53af87c4325
Jon-pol-jons <31860112+Jon-pol-jons@users.noreply.github.com>
2025-03-05 18:43:55 +0300

    Create MyFirstGame.java

---
+++ MyFirstGame.java
@@ -0,0 +1,26 @@
+import java.util.Random;
+
+public class MyFirstGame {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int checkedNumber1 = random.nextInt(100);
+        int result = 1;
+        do {
+            int buf = 50;
+            while (buf > checkedNumber1) {
+                System.out.println(buf + " больше того, что загадал компьютер");
+                result = buf;
+                buf--;
+            }
+            while (buf < checkedNumber1) {
+                System.out.println(buf + " меньше того, что загадал компьютер");
+                result = buf;
+                buf++;
+            }
+            if (buf == checkedNumber1) {
+                System.out.println("Вы победили!");
+            }
+            System.out.println("Вы победили!");
+        } while (result == checkedNumber1);
+    }
+}
