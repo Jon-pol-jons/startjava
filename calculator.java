474f1959ad9dce4d28e91c3b7a804322ea581c61
Jon-pol-jons <31860112+Jon-pol-jons@users.noreply.github.com>
2025-03-05 18:45:12 +0300

    Create calculator.java

---
+++ calculator.java
@@ -0,0 +1,30 @@
+packageimport java.util.Scanner;
+
+public class calculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("ВВедите число А");
+        int checkedNumber1 = scanner.nextInt();
+        System.out.println("ВВедите число В");
+        int checkedNumber2 = scanner.nextInt();
+        System.out.println("Введите операцию над числами");
+        char sumbol = scanner.next().charAt(0);
+        int result = 1;
+        if (sumbol == '+') {
+            result = checkedNumber1 + checkedNumber2;
+        } else if (sumbol == '-') {
+            result = checkedNumber1 - checkedNumber2;
+        } else if (sumbol == '*') {
+            result = checkedNumber1 * checkedNumber2;
+        } else if (sumbol == '/') {
+            result = checkedNumber1 / checkedNumber2;
+        } else if (sumbol == '^') {
+            for (int i = 1; i <= checkedNumber2; i++) {
+                result *= checkedNumber1;
+            }
+        } else if (sumbol == '%') {
+            result = checkedNumber1 % checkedNumber2;
+        }
+        System.out.println(checkedNumber1 + " " + (char) sumbol + " " + checkedNumber2 + " = " + result);
+    }
+}
