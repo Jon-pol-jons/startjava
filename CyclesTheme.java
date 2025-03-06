7131596c74acd0f988c113b5d52748106a738405
Jon-pol-jons <31860112+Jon-pol-jons@users.noreply.github.com>
2025-03-06 15:57:11 +0300

    Update 06.03 CyclesTheme.java

--- CyclesTheme.java
+++ CyclesTheme.java
@@ -1,105 +1,95 @@
+
 public class CyclesTheme {
     public static void main(String[] args) {
         System.out.println("1. ПОДСЧЕТ СУММЫ ЧЕТНЫХ И НЕЧЕТНЫХ ЧИСЕЛ");
-        int checkedNumber1 = -10;
-        int checkedNumber2 = 21;
-        int numberEntries = checkedNumber1;
+        int initialNumber1 = -10;
+        int initialNumber2 = 21;
+        int counter = initialNumber1;
         int evenSum = 0;
         int oddSum = 0;
         do {
-            if (numberEntries % 2 == 0) {
-                evenSum += numberEntries;
+            if (counter % 2 == 0) {
+                evenSum += counter;
             } else {
-                oddSum += numberEntries;
+                oddSum += counter;
             }
-            numberEntries++;
-        } while (numberEntries <= checkedNumber2);
-        System.out.println("В отрезке [" + checkedNumber1 + "," + checkedNumber2 +
-                "] сумма четных чисел = " + evenSum + ",  а нечетных = " +
-                oddSum);
+            counter++;
+        } while (counter <= initialNumber2);
+        System.out.println("В отрезке [" + initialNumber1 + "," + initialNumber2 +
+                "] сумма четных чисел = " + evenSum + ",  а нечетных = " + oddSum);
 
         System.out.println("\n2. ВЫВОД ЧИСЕЛ МЕЖДУ MIN И MAX В ПОРЯДКЕ УБЫВАНИЯ");
-        checkedNumber1 = 10;
-        checkedNumber2 = 5;
+        int checkedNumber1 = 10;
+        int checkedNumber2 = 5;
         int checkedNumber3 = -1;
-        int minNumber = 0;
-        int maxNumber = 0;
-        if (checkedNumber1 > checkedNumber2 && checkedNumber2 > checkedNumber3) {
-            maxNumber = checkedNumber1;
-            minNumber = checkedNumber3;
-        } else if (checkedNumber3 > checkedNumber2 && checkedNumber2 > checkedNumber1) {
-            maxNumber = checkedNumber3;
-            minNumber = checkedNumber1;
-        } else if (checkedNumber2 > checkedNumber3 && checkedNumber3 > checkedNumber1) {
-            maxNumber = checkedNumber2;
-            minNumber = checkedNumber1;
-        } else if (checkedNumber1 > checkedNumber3 && checkedNumber3 > checkedNumber2) {
-            maxNumber = checkedNumber1;
-            minNumber = checkedNumber2;
-        } else if (checkedNumber3 > checkedNumber1 && checkedNumber1 > checkedNumber2) {
-            maxNumber = checkedNumber3;
-            minNumber = checkedNumber2;
-        } else if (checkedNumber2 > checkedNumber1 && checkedNumber1 > checkedNumber3) {
-            maxNumber = checkedNumber2;
-            minNumber = checkedNumber3;
+        int min = checkedNumber1;
+        int max = checkedNumber2;
+        if (checkedNumber1 > checkedNumber2) {
+            min = checkedNumber2;
+            max = checkedNumber1;
         }
-        for (maxNumber -= 1; maxNumber > minNumber; maxNumber--) {
-            System.out.print(maxNumber + " ");
+        if (checkedNumber3 > max) {
+            max = checkedNumber3;
+        }
+        if (checkedNumber3 < min) {
+            min = checkedNumber3;
+        }
+        for (--max; max > min; max--) {
+            System.out.print(max + " ");
         }
 
         System.out.println("\n\n3. ВЫВОД РЕВЕРСИВНОГО ЧИСЛА И СУММЫ ЕГО ЦИФР");
         checkedNumber1 = 1234;
-        checkedNumber2 = checkedNumber1;
+        int currentNumber = checkedNumber1;
+        int sumCheckedNumber = checkedNumber1;
         int reverse = 0;
         int sum = 0;
-        while (checkedNumber2 != 0) {
-            reverse = reverse * 10;
-            reverse = reverse + checkedNumber2 % 10;
-            checkedNumber2 = checkedNumber2 / 10;
+        while (currentNumber > 0) {
+            reverse *= 10;
+            reverse += currentNumber % 10;
+            currentNumber /= 10;
+            sum += sumCheckedNumber % 10;
+            sumCheckedNumber /= 10;
         }
-        int sumCheckedNumber = checkedNumber1;
-        while (sumCheckedNumber != 0) {
-            sum = sum + sumCheckedNumber % 10;
-            sumCheckedNumber = sumCheckedNumber / 10;
-        }
-        System.out.println("number " + reverse);
-        System.out.println("sum " + sum);
+        System.out.println("исходное число в обратном порядке " + reverse);
+        System.out.println("сумма выделенных цифр " + sum);
 
         System.out.println("\n4. ВЫВОД ЧИСЕЛ В НЕСКОЛЬКО СТРОК");
-        checkedNumber1 = 22;
-        checkedNumber2 = 0;
-        int counter = 0;
-        int sumCounter = 0;
-        for (int i = 1; i <= checkedNumber1; i++) {
-            checkedNumber2 = i;
-            if (checkedNumber2 % 2 != 0) {
-                counter += 1;
+        int initialNumber = 23;
+        counter = 0;
+        int maxNumberCounter = 0;
+        for (int i = 1; i <= initialNumber; i++) {
+            if (i % 2 != 0) {
+                counter++;
                 System.out.printf("%4d", i);
             }
-            if (checkedNumber2 % 10 == 0) {
+            if (i % 10 == 0) {
                 System.out.println();
             }
+            maxNumberCounter = counter;
+            while (maxNumberCounter % 5 != 0) {
+                maxNumberCounter++;
+            }
         }
-        char ziro = '0';
-        sumCounter = 15 - counter;
-        while (sumCounter >= 1) {
-            System.out.printf("%4c", ziro);
-            sumCounter--;
+        maxNumberCounter -= counter;
+        while (maxNumberCounter >= 1) {
+            System.out.printf("%4s", 0);
+            maxNumberCounter--;
         }
 
         System.out.println("\n\n5. ПРОВЕРКА КОЛИЧЕСТВА ДВОЕК ЧИСЛА НА ЧЕТНОСТЬ/НЕЧЕТНОСТЬ");
         checkedNumber1 = 3242592;
-        checkedNumber2 = checkedNumber1;
+        currentNumber = checkedNumber1;
         counter = 0;
-        while (checkedNumber2 != 0) {
-            if (checkedNumber2 % 10 == 2) {
+        while (currentNumber > 0) {
+            if (currentNumber % 10 == 2) {
                 counter++;
             }
-            checkedNumber2 = checkedNumber2 / 10;
+            currentNumber /= 10;
         }
         if (counter % 2 == 0) {
             System.out.println("В " + checkedNumber1 + " четное (" + counter + ") количество двоек");
-        } else if (counter % 2 != 0) {
+        } else {
             System.out.println("В " + checkedNumber1 + " не четное (" + counter + ") количество двоек");
         }
 
@@ -168,48 +158,46 @@ public class CyclesTheme {
         }
 
         System.out.println("\n\n8. ПРОВЕРКА, ЯВЛЯЕТСЯ ЛИ ЧИСЛО ПАЛИНДРОМОМ");
-        checkedNumber1 = 1234321;
-        // последняя цифра в числе
-        int digitlast = checkedNumber1 % 10;
-        // первая цифра в числе
-        int digitFirst = checkedNumber1;
+        initialNumber = 1234321;
+        int digitLast = initialNumber % 10;
+        int digitFirst = initialNumber;
         while (digitFirst >= 10) {
-            digitFirst = digitFirst / 10;
+            digitFirst /= 10;
         }
-        if (digitlast == digitFirst) {
-            System.out.println("Число " + checkedNumber1 + " - палиндром");
+        if (digitLast == digitFirst) {
+            System.out.println("Число " + initialNumber + " - палиндром");
         } else {
-            System.out.println("Число " + checkedNumber1 + " - не палиндром");
+            System.out.println("Число " + initialNumber + " - не палиндром");
         }
 
         System.out.println("\n9. ПРОВЕРКА, ЯВЛЯЕТСЯ ЛИ ЧИСЛО СЧАСТЛИВЫМ");
-        checkedNumber1 = 123321;
-        int halfLeftDigits = checkedNumber1;
-        int halfRightDigits = checkedNumber1;
+        initialNumber = 123321;
+        int halfLeftDigits = initialNumber;
+        int halfRightDigits = initialNumber;
         while (halfLeftDigits > 1000) {
             halfLeftDigits /= 1000;
         }
         while (halfRightDigits > 1000) {
-            halfRightDigits %= 1000; 
+            halfRightDigits %= 1000;
         }
         int sumLeftDigits = 0;
         int buf1 = halfLeftDigits;
         while (buf1 > 0) {
-            sumLeftDigits = sumLeftDigits + buf1 % 10;
-            buf1 = buf1 / 10;
+            sumLeftDigits += buf1 % 10;
+            buf1 /= 10;
         }
         int sumRightDigits = 0;
         int buf2 = halfRightDigits;
         while (buf2 > 0) {
-            sumRightDigits = sumRightDigits + buf2 % 10;
-            buf2 = buf2 / 10;
+            sumRightDigits += buf2 % 10;
+            buf2 /= 10;
         }
         if (sumLeftDigits == sumRightDigits) {
-            System.out.println("Число " + checkedNumber1 + " - счастливое");
+            System.out.println("Число " + initialNumber + " - счастливое");
             System.out.println("Сумма цифр  " + halfLeftDigits + " = " + sumLeftDigits);
             System.out.println("Сумма " + halfRightDigits + " = " + sumRightDigits);
         } else {
-            System.out.println("Число " + checkedNumber1 + " - не является счастливым");
+            System.out.println("Число " + initialNumber + " - не является счастливым");
             System.out.println("Сумма цифр " + halfLeftDigits + " = " + sumLeftDigits);
             System.out.println("Сумма " + halfRightDigits + " = " + sumRightDigits);
         }
