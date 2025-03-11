7fd381004ba623d8a2e9f6770e0fdda4d28e9d65
Jon-pol-jons <31860112+Jon-pol-jons@users.noreply.github.com>
2025-03-11 16:30:31 +0300

    Create Person.java

---
+++ Lesson_2/Person.java
@@ -0,0 +1,25 @@
+
+public class Person {
+    String gender = "мужской";
+    String name = "Михаил";
+    int height = 183;
+    int weight = 96;
+    int age = 26;
+    public void go() {
+        System.out.println("Person.go");
+    }
+    public void sit() {
+        System.out.println("Person.sit");
+    }
+    public void run() {
+        System.out.println("Person.run");
+    }
+    public void speak() {
+        System.out.println("Person.speak");
+    }
+    public void learnJava() {
+        System.out.println("Person.learnJava");
+    }
+
+
+}
