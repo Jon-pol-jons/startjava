b042a9daed0e72554a9c0965be4983fb0efe83df
Jon-pol-jons <31860112+Jon-pol-jons@users.noreply.github.com>
2025-03-13 02:36:43 +0300

    Update Person.java

--- Lesson_2/Person.java
+++ Lesson_2/Person.java
@@ -1,3 +1,4 @@
+package Lesson_2;
 
 public class Person {
     String gender = "мужской";
@@ -5,21 +6,24 @@ public class Person {
     int height = 183;
     int weight = 96;
     int age = 26;
+    
     public void go() {
         System.out.println("Person.go");
     }
+
     public void sit() {
         System.out.println("Person.sit");
     }
+
     public void run() {
         System.out.println("Person.run");
     }
+
     public void speak() {
         System.out.println("Person.speak");
     }
+
     public void learnJava() {
         System.out.println("Person.learnJava");
     }
-
-
 }
