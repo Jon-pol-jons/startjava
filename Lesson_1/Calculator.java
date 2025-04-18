7a10216bd9ad1f8d555a1793babc839e96e1e682
Jon-pol-jons <31860112+Jon-pol-jons@users.noreply.github.com>
2025-04-18 15:56:39 +0300

    Update Calculator.java

--- Lesson_1/Calculator.java
+++ Lesson_1/Calculator.java
@@ -1,30 +1,30 @@
 import java.util.Scanner;
 
-public class Calculator {
+public class calculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите число А");
-        int Number1 = scanner.nextInt();
+        int number1 = scanner.nextInt();
         System.out.println("Введите число В");
-        int Number2 = scanner.nextInt();
+        int number2 = scanner.nextInt();
         System.out.println("Введите математическую операцию");
         char symbol = scanner.next().charAt(0);
         int result = 1;
         if (symbol == '+') {
-            result = Number1 + Number2;
+            result = number1 + number2;
         } else if (symbol == '-') {
-            result = Number1 - Number2;
+            result = number1 - number2;
         } else if (symbol == '*') {
-            result = Number1 * Number2;
+            result = number1 * number2;
         } else if (symbol == '/') {
-            result = Number1 / Number2;
+            result = number1 / number2;
         } else if (symbol == '^') {
-            for (int i = 1; i <= Number2; i++) {
-                result *= Number1;
+            for (int i = 1; i <= number2; i++) {
+                result *= number1;
             }
         } else if (symbol == '%') {
-            result = Number1 % Number2;
+            result = number1 % number2;
         }
-        System.out.println(Number1 + " " + symbol + " " + Number2 + " = " + result);
+        System.out.println(number1 + " " + symbol + " " + number2 + " = " + result);
     }
 }
