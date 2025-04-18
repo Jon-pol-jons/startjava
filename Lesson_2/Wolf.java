444e8206e36817e8c869360f3657c6b9930a8361
Jon-pol-jons <31860112+Jon-pol-jons@users.noreply.github.com>
2025-04-18 15:16:40 +0300

    Update Wolf.java

--- Lesson_2/Wolf.java
+++ Lesson_2/Wolf.java
@@ -1,11 +1,53 @@
-package Lesson_2;
-
 public class Wolf {
-    String gender;
-    String name;
-    int weight;
-    int age;
-    String color;
+    private String gender;
+    private String name;
+    private int weight;
+    private int age;
+    private String color;
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age > 8) {
+            System.out.println("Некорректный возраст");
+        } else {
+            this.age = age;
+        }
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
 
     public void go() {
         System.out.println("Идет");
@@ -22,7 +64,7 @@ public class Wolf {
     public void howl() {
         System.out.println("Воет");
     }
-    
+
     public void hunting() {
         System.out.println("Охотится");
     }
