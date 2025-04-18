b5d4d3b4bf5eb2f26ff3f081bffcb10d7c3ced55
Jon-pol-jons <31860112+Jon-pol-jons@users.noreply.github.com>
2025-04-18 16:04:09 +0300

    Create NameMethod.java

---
+++ Lesson_2/NameMethod.java
@@ -0,0 +1,31 @@
+public class NameMethod {
+    public static void main(String[] args) {
+        ContainsBoolean containsBoolean = new ContainsBoolean();
+        containsBoolean.findLongWord();
+        containsBoolean.selectingMenu();
+        containsBoolean.сalculateScore();
+        containsBoolean.searchWord();
+        containsBoolean.error();
+        containsBoolean.synchronization();
+        containsBoolean.recoverData();
+        containsBoolean.pauseDownload();
+        containsBoolean.resetSettings();
+        containsBoolean.dataRecording();
+        containsBoolean.convertTemperature();
+        containsBoolean.mathematicalExpression();
+        containsBoolean.winnerGame();
+        containsBoolean.findBook();
+
+        AbsenceBoolean absenceBoolean = new AbsenceBoolean();
+        absenceBoolean.shouldPerformDivision();
+        absenceBoolean.isFileDeleted();
+        absenceBoolean.hasUniqueNumber();
+        absenceBoolean.hasLatter();
+        absenceBoolean.hasEqualNumber();
+        absenceBoolean.hasResidualAttempt();
+        absenceBoolean.isStringEmpty();
+        absenceBoolean.isEvenNumber();
+        absenceBoolean.isCorrectPath();
+        absenceBoolean.isFileExist();
+    }
+}
