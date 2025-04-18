d3fe10b4d8124034e254234e5c0836b78c4160c4
Jon-pol-jons <31860112+Jon-pol-jons@users.noreply.github.com>
2025-04-18 16:00:37 +0300

    Create ContainsBoolean.java

---
+++ Lesson_2/ContainsBoolean.java
@@ -0,0 +1,73 @@
+public class ContainsBoolean {
+    NameReturn nameReturn = new NameReturn();
+
+    public void findLongWord() {
+        nameReturn.calledMethod();
+        System.out.println("() -> поиск самого длинного слова в предложении из книги по Java");
+    }
+
+    public void selectingMenu() {
+        nameReturn.calledMethod();
+        System.out.println("() -> выбор пункта меню в текстовом редакторе на macOS");
+    }
+
+    public void сalculateScore() {
+        nameReturn.calledMethod();
+        System.out.println("() -> вычисляется среднее значение оценок в школе №1234");
+    }
+
+    public void searchWord() {
+        nameReturn.calledMethod();
+        System.out.println("() -> подсчет количества уникальных слов в Война и мир");
+    }
+
+    public void error() {
+        nameReturn.calledMethod();
+        System.out.println("() -> выводится сообщение об ошибке");
+    }
+
+    public void synchronization() {
+        nameReturn.calledMethod();
+        System.out.println("() -> синхронизация данных с облачным хранилищем");
+    }
+
+    public void recoverData() {
+        nameReturn.calledMethod();
+        System.out.println("() -> востановление данных из резервной копии от 11.03.2024");
+    }
+
+    public void pauseDownload() {
+        nameReturn.calledMethod();
+        System.out.println("() -> приостановление загрузки mp3-файла группы Ария");
+    }
+
+    public void resetSettings() {
+        nameReturn.calledMethod();
+        System.out.println("() -> сброс настроек до заводсих для пылесоса Mi");
+    }
+
+    public void dataRecording() {
+        nameReturn.calledMethod();
+        System.out.println("() -> запись содержимого в файл по указанному пути на флешку");
+    }
+
+    public void convertTemperature() {
+        nameReturn.calledMethod();
+        System.out.println("() -> преобразовывание температуры из Цельсия в Фаренгейт");
+    }
+
+    public void mathematicalExpression() {
+        nameReturn.calledMethod();
+        System.out.println("() -> выводим математическое выражение с тремя аргументами");
+    }
+
+    public void winnerGame() {
+        nameReturn.calledMethod();
+        System.out.println("() -> выявляем победителя среди гонщиков игры \"Need For Speed\"");
+    }
+
+    public void findBook() {
+        nameReturn.calledMethod();
+        System.out.println("() -> найти книгу по имени");
+    }
+}
