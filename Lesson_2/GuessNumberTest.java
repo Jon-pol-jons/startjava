d06b3b568067d79c77dfcffcf0e86daa60ca072e
Jon-pol-jons <31860112+Jon-pol-jons@users.noreply.github.com>
2025-05-14 14:53:43 +0300

    Update GuessNumberTest.java

--- Lesson_2/GuessNumberTest.java
+++ Lesson_2/GuessNumberTest.java
@@ -1,25 +1,31 @@
+package Lesson2;
+
 import java.util.Scanner;
 
 public class GuessNumberTest {
     public static void main(String[] args) {
-        Player player = new Player();
-        GuessNumber guessNumber = new GuessNumber();
         Scanner scanner = new Scanner(System.in);
 
         String userAnswer = "yes";
         while (userAnswer.equals("yes")) {
             userAnswer = "yes";
             try {
-                player.scannerName1();
-                player.scannerName2();
+                System.out.println("Введите имя первого игрока");
+                String name1 = scanner.next();
+                System.out.println("Введите имя второго игрока");
+                String name2 = scanner.next();
+                Player person1 = new Player(name1);
+                Player person2 = new Player(name2);
+                GuessNumber guessNumber = new GuessNumber(name1, name2);
+
                 guessNumber.playGam();
+
                 do {
                     System.out.println("игра завершилась");
                     System.out.print("Хотите продолжить игрнать? [yes/no]: ");
                     userAnswer = scanner.next();
                 } while (!userAnswer.equals("yes") && !userAnswer.equals("no"));
-            } catch (ArrayIndexOutOfBoundsException | NumberFormatException | StringIndexOutOfBoundsException e) {
-                System.out.println("Неправильный формат");
+            } finally {
             }
         }
     }
