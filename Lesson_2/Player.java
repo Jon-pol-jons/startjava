a1ed9c3cddfdde60e71a21be00543ea19310d3c9
Jon-pol-jons <31860112+Jon-pol-jons@users.noreply.github.com>
2025-05-14 14:54:34 +0300

    Update Player.java

--- Lesson_2/Player.java
+++ Lesson_2/Player.java
@@ -1,28 +1,17 @@
-import java.util.Scanner;
+package Lesson2;
 
 public class Player {
-    static private String name1;
-    static private String name2;
+    private String name;
 
-    public String getName1() {
-        return name1;
+    public String getName() {
+        return name;
     }
 
-    public String getName2() {
-        return name2;
+    public void setName(String name) {
+        this.name = name;
     }
 
-    Scanner scanner = new Scanner(System.in);
-
-    public String scannerName1() {
-        System.out.print("Введите имя первого игрока: ");
-        name1 = scanner.next();
-        return name1;
-    }
-
-    public String scannerName2() {
-        System.out.print("Введите имя второго игрока: ");
-        name2 = scanner.next();
-        return name2;
+    public Player(String name) {
+        this.name = name;
     }
 }
