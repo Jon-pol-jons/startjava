c93ea87bd55f97d5d25b9f81a85d4fb1f658a2fd
Jon-pol-jons <31860112+Jon-pol-jons@users.noreply.github.com>
2025-05-14 14:41:18 +0300

    Update AbsenceBoolean.java

--- Lesson_2/AbsenceBoolean.java
+++ Lesson_2/AbsenceBoolean.java
@@ -1,64 +1,66 @@
+package Lesson2;
+
 public class AbsenceBoolean {
-    NameReturn nameReturn = new NameReturn();
+    MeaningMethodName meaningMethodName = new MeaningMethodName();
 
     public boolean shouldPerformDivision() {
         System.out.println();
-        nameReturn.calledMethod();
-        System.out.println("() -> программа выполняет деление или завершается?");
+        System.out.println(meaningMethodName.returningValueMethodName() +
+                "() -> программа выполняет деление или завершается?");
         return false;
     }
 
     public boolean isFileDeleted() {
-        nameReturn.calledMethod();
-        System.out.println("() -> удалился ли файл на жестком диске или флешке?");
+        System.out.println(meaningMethodName.returningValueMethodName() +
+                "() -> удалился ли файл на жестком диске или флешке?");
         return false;
     }
 
     public boolean hasUniqueNumber() {
-        nameReturn.calledMethod();
-        System.out.println("() -> последовательность содержит уникальную цифру?");
+        System.out.println(meaningMethodName.returningValueMethodName() +
+                "() -> последовательность содержит уникальную цифру?");
         return false;
     }
 
     public boolean hasLatter() {
-        nameReturn.calledMethod();
-        System.out.println("() -> пользователь ввел букву или что-то другое?");
+        System.out.println(meaningMethodName.returningValueMethodName() +
+                "() -> пользователь ввел букву или что-то другое?");
         return false;
     }
 
     public boolean hasEqualNumber() {
-        nameReturn.calledMethod();
-        System.out.println("() -> в проверяемых числах, есть равные цифры?");
+        System.out.println(meaningMethodName.returningValueMethodName() +
+                "() -> в проверяемых числах, есть равные цифры?");
         return false;
     }
 
     public boolean hasResidualAttempt() {
-        nameReturn.calledMethod();
-        System.out.println("() -> в игре \"Марио\" остались попытки?");
+        System.out.println(meaningMethodName.returningValueMethodName() +
+                "() -> в игре \"Марио\" остались попытки?");
         return false;
     }
 
     public boolean isStringEmpty() {
-        nameReturn.calledMethod();
-        System.out.println("() -> пользователь ввёл пустую строку или из одних пробелов?");
+        System.out.println(meaningMethodName.returningValueMethodName() +
+                "() -> пользователь ввёл пустую строку или из одних пробелов?");
         return false;
     }
 
     public boolean isEvenNumber() {
-        nameReturn.calledMethod();
-        System.out.println("() -> на кубике, который бросил компьютер, выпало четное число?");
+        System.out.println(meaningMethodName.returningValueMethodName() +
+                "() -> на кубике, который бросил компьютер, выпало четное число?");
         return false;
     }
 
     public boolean isCorrectPath() {
-        nameReturn.calledMethod();
-        System.out.println("() -> путь до файла, который вы ищите на ssd, действительный?");
+        System.out.println(meaningMethodName.returningValueMethodName() +
+                "() -> путь до файла, который вы ищите на ssd, действительный?");
         return false;
     }
 
     public boolean isFileExist() {
-        nameReturn.calledMethod();
-        System.out.println("() -> файл по указанному адресу существует?");
+        System.out.println(meaningMethodName.returningValueMethodName() +
+                "() -> файл по указанному адресу существует?");
         return false;
     }
 }
