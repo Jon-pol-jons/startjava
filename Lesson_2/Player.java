705970f404ca65d5f2a8df4326841938606f2a32
Jon-pol-jons <31860112+Jon-pol-jons@users.noreply.github.com>
2025-05-19 13:42:53 +0300

    Update Player.java

--- Lesson_2/Player.java
+++ Lesson_2/Player.java
@@ -3,15 +3,11 @@ package Lesson2;
 public class Player {
     private String name;
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public Player(String name) {
         this.name = name;
+    }
+
+    public String getName() {
+        return name;
     }
 }
