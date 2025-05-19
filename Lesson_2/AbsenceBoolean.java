4648301931803eaea07b622ccb25a0e9a09c0e14
Jon-pol-jons <31860112+Jon-pol-jons@users.noreply.github.com>
2025-05-19 14:02:14 +0300

    Update AbsenceBoolean.java

--- Lesson_2/AbsenceBoolean.java
+++ Lesson_2/AbsenceBoolean.java
@@ -1,66 +1,65 @@
 package Lesson2;
 
 public class AbsenceBoolean {
-    MeaningMethodName meaningMethodName = new MeaningMethodName();
-
+    boolean yes = true;
+    boolean no = false;
     public boolean shouldPerformDivision() {
-        System.out.println();
-        System.out.println(meaningMethodName.returningValueMethodName() +
-                "() -> программа выполняет деление или завершается?");
-        return false;
+        System.out.println("\n" + MeaningMethodName.returningValueMethodName() +
+                "() -> программа выполняет деление " + yes + " или завершается? " + no );
+        return true;
     }
 
     public boolean isFileDeleted() {
-        System.out.println(meaningMethodName.returningValueMethodName() +
-                "() -> удалился ли файл на жестком диске или флешке?");
+        System.out.println(MeaningMethodName.returningValueMethodName() +
+                "() -> удалился ли файл на жестком диске или флешке? " + yes);
         return false;
     }
 
     public boolean hasUniqueNumber() {
-        System.out.println(meaningMethodName.returningValueMethodName() +
-                "() -> последовательность содержит уникальную цифру?");
+        System.out.println(MeaningMethodName.returningValueMethodName() +
+                "() -> последовательность содержит уникальную цифру? " + no);
         return false;
     }
 
     public boolean hasLatter() {
-        System.out.println(meaningMethodName.returningValueMethodName() +
-                "() -> пользователь ввел букву или что-то другое?");
+        System.out.println(MeaningMethodName.returningValueMethodName() +
+                "() -> пользователь ввел букву " + no + " или что-то другое?" + yes);
         return false;
     }
 
     public boolean hasEqualNumber() {
-        System.out.println(meaningMethodName.returningValueMethodName() +
-                "() -> в проверяемых числах, есть равные цифры?");
+        System.out.println(MeaningMethodName.returningValueMethodName() +
+                "() -> в проверяемых числах, есть равные цифры? " + yes);
         return false;
     }
 
     public boolean hasResidualAttempt() {
-        System.out.println(meaningMethodName.returningValueMethodName() +
-                "() -> в игре \"Марио\" остались попытки?");
+        System.out.println(MeaningMethodName.returningValueMethodName() +
+                "() -> в игре \"Марио\" остались попытки? " + no);
         return false;
     }
 
     public boolean isStringEmpty() {
-        System.out.println(meaningMethodName.returningValueMethodName() +
-                "() -> пользователь ввёл пустую строку или из одних пробелов?");
+        System.out.println(MeaningMethodName.returningValueMethodName() +
+                "() -> пользователь ввёл пустую строку " + yes + " или из одних пробелов? " + no);
         return false;
     }
 
     public boolean isEvenNumber() {
-        System.out.println(meaningMethodName.returningValueMethodName() +
-                "() -> на кубике, который бросил компьютер, выпало четное число?");
+        System.out.println(MeaningMethodName.returningValueMethodName() +
+                "() -> на кубике, который бросил компьютер, выпало четное число? " + no);
         return false;
     }
 
     public boolean isCorrectPath() {
-        System.out.println(meaningMethodName.returningValueMethodName() +
-                "() -> путь до файла, который вы ищите на ssd, действительный?");
+        System.out.println(MeaningMethodName.returningValueMethodName() +
+                "() -> путь до файла, который вы ищите на ssd, действительный? " + yes);
         return false;
     }
 
     public boolean isFileExist() {
-        System.out.println(meaningMethodName.returningValueMethodName() +
-                "() -> файл по указанному адресу существует?");
+        System.out.println(MeaningMethodName.returningValueMethodName() +
+                "() -> файл по указанному адресу существует? " + yes);
         return false;
     }
 }
