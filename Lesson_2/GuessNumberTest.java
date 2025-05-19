4c2f21606a9a4fa11edeb71c3a81fc323444fe82
Jon-pol-jons <31860112+Jon-pol-jons@users.noreply.github.com>
2025-05-19 13:43:17 +0300

    Update GuessNumberTest.java

--- Lesson_2/GuessNumberTest.java
+++ Lesson_2/GuessNumberTest.java
@@ -5,28 +5,22 @@ import java.util.Scanner;
 public class GuessNumberTest {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        String userAnswer = "yes";
-        while (userAnswer.equals("yes")) {
-            userAnswer = "yes";
-            try {
-                System.out.println("Введите имя первого игрока");
-                String name1 = scanner.next();
-                System.out.println("Введите имя второго игрока");
-                String name2 = scanner.next();
-                Player person1 = new Player(name1);
-                Player person2 = new Player(name2);
-                GuessNumber guessNumber = new GuessNumber(name1, name2);
-
-                guessNumber.playGam();
-
-                do {
-                    System.out.println("игра завершилась");
-                    System.out.print("Хотите продолжить игрнать? [yes/no]: ");
-                    userAnswer = scanner.next();
-                } while (!userAnswer.equals("yes") && !userAnswer.equals("no"));
-            } finally {
-            }
+        String playerAnswer = "yes";
+        while (playerAnswer.equals("yes")) {
+            System.out.println("Введите имя первого игрока");
+            String name1 = scanner.next();
+            System.out.println("Введите имя второго игрока");
+            String name2 = scanner.next();
+            Player person1 = new Player(name1);
+            Player person2 = new Player(name2);
+            GuessNumber guessNumber = new GuessNumber(person1, person2);
+            guessNumber.playGam();
+            do {
+                System.out.println("игра завершилась");
+                System.out.print("Хотите продолжить играть? [yes/no]: ");
+                playerAnswer = scanner.next();
+            } while (!playerAnswer.equals("yes") && !playerAnswer.equals("no"));
         }
     }
 }
+
