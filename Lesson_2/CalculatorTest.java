f27fbc40b986ca4230faf3d953b4a670bcd118a6
Jon-pol-jons <31860112+Jon-pol-jons@users.noreply.github.com>
2025-05-19 13:59:15 +0300

    Update CalculatorTest.java

--- Lesson_2/CalculatorTest.java
+++ Lesson_2/CalculatorTest.java
@@ -8,24 +8,21 @@ public class CalculatorTest {
         Calculator calculator = new Calculator();
         String userAnswer = "yes";
         while (userAnswer.equals("yes")) {
-            userAnswer = "yes";
-            try {
-                System.out.print("Введите первое число: ");
-                int number1 = scanner.nextInt();
-                calculator.setNumber1(number1);
-                System.out.print("Введите второе число: ");
-                int number2 = scanner.nextInt();
-                calculator.setNumber2(number2);
-                System.out.print("Введите знак операции (+, -, *, /, ^, %): ");
-                char matSign = scanner.next().charAt(0);
-                calculator.setMatSign(matSign);
-                System.out.println(calculator.calculate(number1, number2));
-                do {
-                    System.out.print("Хотите продолжить вычисления? [yes/no]: ");
-                    userAnswer = scanner.next();
-                } while (!userAnswer.equals("yes") && !userAnswer.equals("no"));
-            } finally {
-            }
+            System.out.print("Введите первое число: ");
+            int number1 = scanner.nextInt();
+            calculator.setNumber1(number1);
+            System.out.print("Введите второе число: ");
+            int number2 = scanner.nextInt();
+            calculator.setNumber2(number2);
+            System.out.print("Введите знак операции (+, -, *, /, ^, %): ");
+            char mathSign = scanner.next().charAt(0);
+            calculator.setMatSign(mathSign);
+            System.out.println(calculator.calculate());
+            do {
+                System.out.print("Хотите продолжить вычисления? [yes/no]: ");
+                userAnswer = scanner.next();
+            } while (!userAnswer.equals("yes") && !userAnswer.equals("no"));
         }
     }
 }
+
