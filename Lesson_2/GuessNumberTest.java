67b355c1543a1d5487fbcf61ac1019bac603848b
Jon-pol-jons <31860112+Jon-pol-jons@users.noreply.github.com>
2025-05-26 13:50:33 +0300

    Update GuessNumberTest.java

--- Lesson_2/GuessNumberTest.java
+++ Lesson_2/GuessNumberTest.java
@@ -11,10 +11,10 @@ public class GuessNumberTest {
             String name1 = scanner.next();
             System.out.println("Введите имя второго игрока");
             String name2 = scanner.next();
-            Player person1 = new Player(name1);
-            Player person2 = new Player(name2);
-            GuessNumber guessNumber = new GuessNumber(person1, person2);
-            guessNumber.playGam();
+            Player player1 = new Player(name1);
+            Player player2 = new Player(name2);
+            GuessNumber game = new GuessNumber(player1, player2);
+            game.play();
             do {
                 System.out.println("игра завершилась");
                 System.out.print("Хотите продолжить играть? [yes/no]: ");
