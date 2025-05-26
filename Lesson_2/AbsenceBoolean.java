e78e11846eb760bd8ad5ea779613910ea79f4560
Jon-pol-jons <31860112+Jon-pol-jons@users.noreply.github.com>
2025-05-26 13:17:46 +0300

    Update AbsenceBoolean.java

--- Lesson_2/AbsenceBoolean.java
+++ Lesson_2/AbsenceBoolean.java
@@ -1,65 +1,73 @@
 package Lesson2;
 
 public class AbsenceBoolean {
-    boolean yes = true;
-    boolean no = false;
-    public boolean shouldPerformDivision() {
-        System.out.println("\n" + MeaningMethodName.returningValueMethodName() +
-                "() -> программа выполняет деление " + yes + " или завершается? " + no );
-        return true;
+    public void findLongWord() {
+        System.out.println(MethodName.getCurrentName() +
+                "() -> поиск самого длинного слова в предложении из книги по Java");
     }
 
-    public boolean isFileDeleted() {
-        System.out.println(MeaningMethodName.returningValueMethodName() +
-                "() -> удалился ли файл на жестком диске или флешке? " + yes);
-        return false;
+    public void selectingMenu() {
+        System.out.println(MethodName.getCurrentName() +
+                "() -> выбор пункта меню в текстовом редакторе на macOS");
     }
 
-    public boolean hasUniqueNumber() {
-        System.out.println(MeaningMethodName.returningValueMethodName() +
-                "() -> последовательность содержит уникальную цифру? " + no);
-        return false;
+    public void сalculateScore() {
+        System.out.println(MethodName.getCurrentName() +
+                "() -> вычисляется среднее значение оценок в школе №1234");
     }
 
-    public boolean hasLatter() {
-        System.out.println(MeaningMethodName.returningValueMethodName() +
-                "() -> пользователь ввел букву " + no + " или что-то другое?" + yes);
-        return false;
+    public void searchWord() {
+        System.out.println(MethodName.getCurrentName() +
+                "() -> подсчет количества уникальных слов в Война и мир");
     }
 
-    public boolean hasEqualNumber() {
-        System.out.println(MeaningMethodName.returningValueMethodName() +
-                "() -> в проверяемых числах, есть равные цифры? " + yes);
-        return false;
+    public void error() {
+        System.out.println(MethodName.getCurrentName() +
+                "() -> выводится сообщение об ошибке");
     }
 
-    public boolean hasResidualAttempt() {
-        System.out.println(MeaningMethodName.returningValueMethodName() +
-                "() -> в игре \"Марио\" остались попытки? " + no);
-        return false;
+    public void synchronization() {
+        System.out.println(MethodName.getCurrentName() +
+                "() -> синхронизация данных с облачным хранилищем");
     }
 
-    public boolean isStringEmpty() {
-        System.out.println(MeaningMethodName.returningValueMethodName() +
-                "() -> пользователь ввёл пустую строку " + yes + " или из одних пробелов? " + no);
-        return false;
+    public void recoverData() {
+        System.out.println(MethodName.getCurrentName() +
+                "() -> восстановление данных из резервной копии от 11.03.2024");
     }
 
-    public boolean isEvenNumber() {
-        System.out.println(MeaningMethodName.returningValueMethodName() +
-                "() -> на кубике, который бросил компьютер, выпало четное число? " + no);
-        return false;
+    public void pauseDownload() {
+        System.out.println(MethodName.getCurrentName() +
+                "() -> приостановление загрузки mp3-файла группы Ария");
     }
 
-    public boolean isCorrectPath() {
-        System.out.println(MeaningMethodName.returningValueMethodName() +
-                "() -> путь до файла, который вы ищите на ssd, действительный? " + yes);
-        return false;
+    public void resetSettings() {
+        System.out.println(MethodName.getCurrentName() +
+                "() -> сброс настроек до заводских для пылесоса Mi");
     }
 
-    public boolean isFileExist() {
-        System.out.println(MeaningMethodName.returningValueMethodName() +
-                "() -> файл по указанному адресу существует? " + yes);
-        return false;
+    public void dataRecording() {
+        System.out.println(MethodName.getCurrentName() +
+                "() -> запись содержимого в файл по указанному пути на флешку");
+    }
+
+    public void convertTemperature() {
+        System.out.println(MethodName.getCurrentName() +
+                "() -> преобразовывание температуры из Цельсия в Фаренгейт");
+    }
+
+    public void mathematicalExpression() {
+        System.out.println(MethodName.getCurrentName() +
+                "() -> выводим математическое выражение с тремя аргументами");
+    }
+
+    public void winnerGame() {
+        System.out.println(MethodName.getCurrentName() +
+                "() -> выявляем победителя среди гонщиков игры \"Need For Speed\"");
+    }
+
+    public void findBook() {
+        System.out.println(MethodName.getCurrentName() +
+                "() -> найти книгу по имени");
     }
 }
