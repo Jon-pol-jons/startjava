1312ff66906f4ff00a66d4af0dedfb9d14839716
Jon-pol-jons <31860112+Jon-pol-jons@users.noreply.github.com>
2025-05-28 14:06:13 +0300

    Update MethodName.java

--- Lesson_2/MethodName.java
+++ Lesson_2/MethodName.java
@@ -1,9 +1,7 @@
 package Lesson2;
 
 public class MethodName {
-    private static String methodName;
-
     public static String getCurrentName() {
-        return methodName = Thread.currentThread().getStackTrace()[2].getMethodName();
+        return Thread.currentThread().getStackTrace()[2].getMethodName();
     }
 }
