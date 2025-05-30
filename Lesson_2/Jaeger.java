bd14a4344bf27beb5f1448952c34462dfb9a791b
Jon-pol-jons <31860112+Jon-pol-jons@users.noreply.github.com>
2025-05-30 18:19:45 +0300

    Update Jaeger.java

--- Lesson_2/Jaeger.java
+++ Lesson_2/Jaeger.java
@@ -1,11 +1,25 @@
+package Lesson2;
+
 public class Jaeger {
     private String name;
     private int height;
     private String mark;
     private String status;
+    private float haightChernoAlpha;
+    private float ft = 0.3048F;
+
+    public Jaeger() {
+    }
+
+    public Jaeger(int height, String mark, String status, String name) {
+        this.height = height;
+        this.mark = mark;
+        this.status = status;
+        this.name = name;
+    }
 
     public void setName(String name) {
-        this.height = height;
+        this.name = name;
     }
 
     public String getName() {
@@ -36,23 +50,13 @@ public class Jaeger {
         return status;
     }
 
-    public Jaeger(int height, String mark, String status, String name) {
-        this.height = height;
-        this.mark = mark;
-        this.status = status;
-        this.name = name;
-    }
-
-    public void jaeger1() {
+    public void runReactor() {
         System.out.println("jaeger " + name + " запускает реактор");
     }
 
-    private float haightChernoAlpha;
-    private float ft = 0.3048F;
-
-    public float haightJaeger() {
+    public float findHeight() {
         haightChernoAlpha = height * ft;
-        System.out.println("Высота в метрах егеря " + name + " составляет " + haightChernoAlpha);
+        System.out.println("Высота егеря в метрах " + name + " составляет " + haightChernoAlpha);
         return haightChernoAlpha;
     }
 }
