252d3f2017dcf20f2b9c0d15f3b3ea17f468849b
Jon-pol-jons <31860112+Jon-pol-jons@users.noreply.github.com>
2025-05-30 18:26:24 +0300

    Update AbsenceBoolean.java

--- Lesson_2/AbsenceBoolean.java
+++ Lesson_2/AbsenceBoolean.java
@@ -1,73 +1,73 @@
 package Lesson2;
 
 public class AbsenceBoolean {
-    public void findTheLongWord() {
-        System.out.println(MethodName.getCurrentName() +
+    public void findTheLongestWord() {
+        System.out.println(MethodUtil.getCurrentName() +
                 "() -> поиск самого длинного слова в предложении из книги по Java");
     }
 
-    public void selectItemMenu() {
-        System.out.println(MethodName.getCurrentName() +
+    public void selectMenuItem() {
+        System.out.println(MethodUtil.getCurrentName() +
                 "() -> выбор пункта меню в текстовом редакторе на macOS");
     }
 
     public void сalculateAverageScore() {
-        System.out.println(MethodName.getCurrentName() +
+        System.out.println(MethodUtil.getCurrentName() +
                 "() -> вычисляется среднее значение оценок в школе №1234");
     }
 
-    public void countingUniqueWords() {
-        System.out.println(MethodName.getCurrentName() +
+    public void countUniqueWords() {
+        System.out.println(MethodUtil.getCurrentName() +
                 "() -> подсчет количества уникальных слов в Война и мир");
     }
 
-    public void errorMessage() {
-        System.out.println(MethodName.getCurrentName() +
+    public void showErrorMessage() {
+        System.out.println(MethodUtil.getCurrentName() +
                 "() -> выводится сообщение об ошибке");
     }
 
-    public void syncCloudStorage() {
-        System.out.println(MethodName.getCurrentName() +
+    public void syncCloud() {
+        System.out.println(MethodUtil.getCurrentName() +
                 "() -> синхронизация данных с облачным хранилищем");
     }
 
     public void recoverData() {
-        System.out.println(MethodName.getCurrentName() +
+        System.out.println(MethodUtil.getCurrentName() +
                 "() -> восстановление данных из резервной копии от 11.03.2024");
     }
 
     public void pauseDownload() {
-        System.out.println(MethodName.getCurrentName() +
+        System.out.println(MethodUtil.getCurrentName() +
                 "() -> приостановление загрузки mp3-файла группы Ария");
     }
 
     public void resetSettings() {
-        System.out.println(MethodName.getCurrentName() +
+        System.out.println(MethodUtil.getCurrentName() +
                 "() -> сброс настроек до заводских для пылесоса Mi");
     }
 
-    public void writeСontentFile() {
-        System.out.println(MethodName.getCurrentName() +
+    public void writeToFile() {
+        System.out.println(MethodUtil.getCurrentName() +
                 "() -> запись содержимого в файл по указанному пути на флешку");
     }
 
     public void convertTemperature() {
-        System.out.println(MethodName.getCurrentName() +
+        System.out.println(MethodUtil.getCurrentName() +
                 "() -> преобразование температуры из Цельсия в Фаренгейт");
     }
 
-    public void mathExpression() {
-        System.out.println(MethodName.getCurrentName() +
+    public void renderMathExpression() {
+        System.out.println(MethodUtil.getCurrentName() +
                 "() -> выводим математическое выражение с тремя аргументами");
     }
 
-    public void findWinnerGame() {
-        System.out.println(MethodName.getCurrentName() +
+    public void findWinner() {
+        System.out.println(MethodUtil.getCurrentName() +
                 "() -> выявляем победителя среди гонщиков игры \"Need For Speed\"");
     }
 
-    public void findBook() {
-        System.out.println(MethodName.getCurrentName() +
+    public void findBookByName() {
+        System.out.println(MethodUtil.getCurrentName() +
                 "() -> найти книгу по имени");
     }
 }
