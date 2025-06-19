63f83205e0c4bf44ae4cf43270d2795b0c088938
Semenchuk <semenchuk49@mail.ru>
2025-06-19 15:51:25 +0300

    пареименование пакетов

--- src/com/startjava/lesson_2_3_4/method/naming/MethodUtil.java
+++ src/com/startjava/lesson_2_3_4/method/naming/MethodUtil.java
@@ -1,4 +1,4 @@
-package com.startjava.lesson_2_3.method.naming;
+package com.startjava.lesson_2_3_4.method.naming;
 public class MethodUtil {
     public static String getCurrentName() {
         return Thread.currentThread().getStackTrace()[2].getMethodName();
